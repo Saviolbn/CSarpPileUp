4c338e00d5572fdc29ae4ff1d1536e1a4cee879f
Saviolbn <saviolbn@gmail.com>
2024-04-05 18:06:14 -0300

    mover topo funcional

--- Program.cs
+++ Program.cs
@@ -6,22 +6,112 @@
 // Só pode mover um disco por vez
 
 // Será usado o algoritmo de busca A*
+// Regras:
+// R1 mover A para B
+// R2 mover A para C
+// R3 mover B para A
+// R4 mover B para C
+// R5 mover C para A
+// R6 mover C para B
 
-int numDiscos = 5;
-int[] torreA= new int[numDiscos];
-int[] torreB= new int[numDiscos];
-int[] torreC= new int[numDiscos];
 
-
-void inicializarTorre()
+class Program
 {
-    for (int i = numDiscos - 1; i >= 0; i--)
+    static void Main(string[] args)
     {
-        torreA[i] = i;
+        int numDiscos = 5;
+        int[] torreA = new int[numDiscos];
+        int[] torreB = new int[numDiscos];
+        int[] torreC = new int[numDiscos];
+
+
+        void inicializarTorre()
+        {
+            for (int i = 0; i < numDiscos; i++)
+            {
+                torreA[i] = numDiscos - i;
+            }
+        };
+
+        // quanto menor o valor retornado, melhor estamos
+        int avaliarSistema()
+        {
+            int h = 0;
+
+            for (int i = numDiscos; i > 0; i--)
+            {
+                if (torreC[i - 1] != i)
+                {
+                    h++;
+                }
+            }
+
+            return h;
+        }
+
+
+
+        // move do topo da torreX para o fundo da torre Y
+        bool moverTopo(int[] torreX, int[] torreY)
+        {
+            int topoIndex = -1;
+            for (int i = numDiscos - 1; i >= 0; i--)
+            {
+                if(torreX[i] != 0) 
+                {
+                    topoIndex = i;
+                    break;
+                }
+            }
+
+            if (topoIndex == -1) 
+            {
+                return false;
+            } 
+
+            Console.WriteLine(topoIndex);
+
+
+            for (int i = 0; i < numDiscos - 1; i++)
+            {
+                if(i == 0) 
+                {
+                    if (torreY[i] == 0) 
+                    {
+                        torreY[i] = torreX[topoIndex];
+                        torreX[topoIndex] = 0;
+                    }
+                } else {               
+                    if (torreY[i] == 0 && torreY[i - 1] > torreX[topoIndex])
+                    {
+                        torreY[i] = torreX[topoIndex];
+                        torreX[topoIndex] = 0;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        void imprimirSistema()
+        {
+            Console.WriteLine($"{string.Join("", torreA)} {string.Join("", torreB)} {string.Join("", torreC)}");
+        }
+
+
+        inicializarTorre();
+        imprimirSistema();
+
+        moverTopo(torreA, torreC);
+        imprimirSistema();
+
+        moverTopo(torreA, torreC);
+        imprimirSistema();
+
+        moverTopo(torreA, torreB);
+        imprimirSistema();
+
+        moverTopo(torreC, torreB);
+        imprimirSistema();
     }
-};
-
-inicializarTorre();
-Console.WriteLine($"{string.Join("",torreA)} {string.Join("",torreB)} {string.Join("",torreC)}");
-
-Console.WriteLine("Hello, World!");
+}
